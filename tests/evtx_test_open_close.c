22b902ebfb8c1d275fc379e29a5ee391acb190cd
Joachim Metz <joachim.metz@gmail.com>
2016-01-29 09:52:06 +0100

    Worked on tests.

--- tests/evtx_test_open_close.c
+++ tests/evtx_test_open_close.c
@@ -20,12 +20,11 @@
  */
 
 #include <common.h>
+#include <file_stream.h>
 
 #if defined( HAVE_STDLIB_H ) || defined( WINAPI )
 #include <stdlib.h>
 #endif
-
-#include <stdio.h>
 
 #include "evtx_test_libcerror.h"
 #include "evtx_test_libcstring.h"
