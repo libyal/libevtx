36c3f90ee51bb67ba80a61c5d7cd3cad4a7f5a13
Joachim Metz <joachim.metz@gmail.com>
2018-04-01 22:09:12 +0200

    Applied updates

--- tests/evtx_test_notify.c
+++ tests/evtx_test_notify.c
@@ -27,6 +27,7 @@
 #include <stdlib.h>
 #endif
 
+#include "evtx_test_libcerror.h"
 #include "evtx_test_libevtx.h"
 #include "evtx_test_macros.h"
 #include "evtx_test_unused.h"
@@ -51,13 +52,37 @@ int evtx_test_notify_set_verbose(
 int evtx_test_notify_set_stream(
      void )
 {
-	/* Test invocation of function only
+	libcerror_error_t *error = NULL;
+	int result               = 0;
+
+	/* Test regular cases
 	 */
-	libevtx_notify_set_stream(
-	 NULL,
-	 NULL );
+	result = libevtx_notify_set_stream(
+	          NULL,
+	          &error );
+
+	EVTX_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+	EVTX_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	/* Test error cases
+	 */
+/* TODO test libcnotify_stream_set failure */
 
 	return( 1 );
+
+on_error:
+	if( error != NULL )
+	{
+		libcerror_error_free(
+		 &error );
+	}
+	return( 0 );
 }
 
 /* Tests the libevtx_notify_stream_open function
@@ -66,13 +91,65 @@ int evtx_test_notify_set_stream(
 int evtx_test_notify_stream_open(
      void )
 {
-	/* Test invocation of function only
+	libcerror_error_t *error = NULL;
+	int result               = 0;
+
+	/* Test regular cases
 	 */
-	libevtx_notify_stream_open(
-	 NULL,
-	 NULL );
+	result = libevtx_notify_stream_open(
+	          "notify_stream.log",
+	          &error );
+
+	EVTX_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+	EVTX_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	/* Test error cases
+	 */
+	result = libevtx_notify_stream_open(
+	          NULL,
+	          &error );
+
+	EVTX_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 -1 );
+
+	EVTX_TEST_ASSERT_IS_NOT_NULL(
+	 "error",
+	 error );
+
+	libcerror_error_free(
+	 &error );
+
+	/* Clean up
+	 */
+	result = libevtx_notify_stream_close(
+	          &error );
+
+	EVTX_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 0 );
+
+	EVTX_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
 
 	return( 1 );
+
+on_error:
+	if( error != NULL )
+	{
+		libcerror_error_free(
+		 &error );
+	}
+	return( 0 );
 }
 
 /* Tests the libevtx_notify_stream_close function
@@ -81,12 +158,36 @@ int evtx_test_notify_stream_open(
 int evtx_test_notify_stream_close(
      void )
 {
-	/* Test invocation of function only
+	libcerror_error_t *error = NULL;
+	int result               = 0;
+
+	/* Test regular cases
 	 */
-	libevtx_notify_stream_close(
-	 NULL );
+	result = libevtx_notify_stream_close(
+	          &error );
+
+	EVTX_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 0 );
+
+	EVTX_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	/* Test error cases
+	 */
+/* TODO test libcnotify_stream_close failure */
 
 	return( 1 );
+
+on_error:
+	if( error != NULL )
+	{
+		libcerror_error_free(
+		 &error );
+	}
+	return( 0 );
 }
 
 /* The main program
--- tests/evtx_test_support.c
+++ tests/evtx_test_support.c
@@ -36,6 +36,7 @@
 #include "evtx_test_libcerror.h"
 #include "evtx_test_libevtx.h"
 #include "evtx_test_macros.h"
+#include "evtx_test_memory.h"
 #include "evtx_test_unused.h"
 
 #if !defined( LIBEVTX_HAVE_BFIO )
@@ -209,38 +210,40 @@ int evtx_test_check_file_signature(
 	libcerror_error_t *error = NULL;
 	int result               = 0;
 
-	/* Initialize test
-	 */
-	result = evtx_test_get_narrow_source(
-	          source,
-	          narrow_source,
-	          256,
-	          &error );
+	if( source != NULL )
+	{
+		/* Initialize test
+		 */
+		result = evtx_test_get_narrow_source(
+		          source,
+		          narrow_source,
+		          256,
+		          &error );
 
-	EVTX_TEST_ASSERT_EQUAL_INT(
-	 "result",
-	 result,
-	 1 );
+		EVTX_TEST_ASSERT_EQUAL_INT(
+		 "result",
+		 result,
+		 1 );
 
-	EVTX_TEST_ASSERT_IS_NULL(
-	 "error",
-	 error );
+		EVTX_TEST_ASSERT_IS_NULL(
+		 "error",
+		 error );
 
-	/* Test check file signature
-	 */
-	result = libevtx_check_file_signature(
-	          narrow_source,
-	          &error );
+		/* Test check file signature
+		 */
+		result = libevtx_check_file_signature(
+		          narrow_source,
+		          &error );
 
-	EVTX_TEST_ASSERT_EQUAL_INT(
-	 "result",
-	 result,
-	 1 );
+		EVTX_TEST_ASSERT_EQUAL_INT(
+		 "result",
+		 result,
+		 1 );
 
-	EVTX_TEST_ASSERT_IS_NULL(
-	 "error",
-	 error );
-
+		EVTX_TEST_ASSERT_IS_NULL(
+		 "error",
+		 error );
+	}
 	/* Test error cases
 	 */
 	result = libevtx_check_file_signature(
@@ -259,6 +262,54 @@ int evtx_test_check_file_signature(
 	libcerror_error_free(
 	 &error );
 
+	result = libevtx_check_file_signature(
+	          "",
+	          &error );
+
+	EVTX_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 -1 );
+
+	EVTX_TEST_ASSERT_IS_NOT_NULL(
+	 "error",
+	 error );
+
+	libcerror_error_free(
+	 &error );
+
+	if( source != NULL )
+	{
+#if defined( HAVE_EVTX_TEST_MEMORY )
+
+		/* Test libevtx_check_file_signature with malloc failing in libbfio_file_initialize
+		 */
+		evtx_test_malloc_attempts_before_fail = 0;
+
+		result = libevtx_check_file_signature(
+		          narrow_source,
+		          &error );
+
+		if( evtx_test_malloc_attempts_before_fail != -1 )
+		{
+			evtx_test_malloc_attempts_before_fail = -1;
+		}
+		else
+		{
+			EVTX_TEST_ASSERT_EQUAL_INT(
+			 "result",
+			 result,
+			 -1 );
+
+			EVTX_TEST_ASSERT_IS_NOT_NULL(
+			 "error",
+			 error );
+
+			libcerror_error_free(
+			 &error );
+		}
+#endif /* defined( HAVE_EVTX_TEST_MEMORY ) */
+	}
 	return( 1 );
 
 on_error:
@@ -283,38 +334,40 @@ int evtx_test_check_file_signature_wide(
 	libcerror_error_t *error = NULL;
 	int result               = 0;
 
-	/* Initialize test
-	 */
-	result = evtx_test_get_wide_source(
-	          source,
-	          wide_source,
-	          256,
-	          &error );
+	if( source != NULL )
+	{
+		/* Initialize test
+		 */
+		result = evtx_test_get_wide_source(
+		          source,
+		          wide_source,
+		          256,
+		          &error );
 
-	EVTX_TEST_ASSERT_EQUAL_INT(
-	 "result",
-	 result,
-	 1 );
+		EVTX_TEST_ASSERT_EQUAL_INT(
+		 "result",
+		 result,
+		 1 );
 
-	EVTX_TEST_ASSERT_IS_NULL(
-	 "error",
-	 error );
+		EVTX_TEST_ASSERT_IS_NULL(
+		 "error",
+		 error );
 
-	/* Test check file signature
-	 */
-	result = libevtx_check_file_signature_wide(
-	          wide_source,
-	          &error );
+		/* Test check file signature
+		 */
+		result = libevtx_check_file_signature_wide(
+		          wide_source,
+		          &error );
 
-	EVTX_TEST_ASSERT_EQUAL_INT(
-	 "result",
-	 result,
-	 1 );
+		EVTX_TEST_ASSERT_EQUAL_INT(
+		 "result",
+		 result,
+		 1 );
 
-	EVTX_TEST_ASSERT_IS_NULL(
-	 "error",
-	 error );
-
+		EVTX_TEST_ASSERT_IS_NULL(
+		 "error",
+		 error );
+	}
 	/* Test error cases
 	 */
 	result = libevtx_check_file_signature_wide(
@@ -333,6 +386,54 @@ int evtx_test_check_file_signature_wide(
 	libcerror_error_free(
 	 &error );
 
+	result = libevtx_check_file_signature_wide(
+	          L"",
+	          &error );
+
+	EVTX_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 -1 );
+
+	EVTX_TEST_ASSERT_IS_NOT_NULL(
+	 "error",
+	 error );
+
+	libcerror_error_free(
+	 &error );
+
+	if( source != NULL )
+	{
+#if defined( HAVE_EVTX_TEST_MEMORY )
+
+		/* Test libevtx_check_file_signature_wide with malloc failing in libbfio_file_initialize
+		 */
+		evtx_test_malloc_attempts_before_fail = 0;
+
+		result = libevtx_check_file_signature_wide(
+		          wide_source,
+		          &error );
+
+		if( evtx_test_malloc_attempts_before_fail != -1 )
+		{
+			evtx_test_malloc_attempts_before_fail = -1;
+		}
+		else
+		{
+			EVTX_TEST_ASSERT_EQUAL_INT(
+			 "result",
+			 result,
+			 -1 );
+
+			EVTX_TEST_ASSERT_IS_NOT_NULL(
+			 "error",
+			 error );
+
+			libcerror_error_free(
+			 &error );
+		}
+#endif /* defined( HAVE_EVTX_TEST_MEMORY ) */
+	}
 	return( 1 );
 
 on_error:
@@ -352,7 +453,7 @@ on_error:
 int evtx_test_check_file_signature_file_io_handle(
      const system_character_t *source )
 {
-	uint8_t empty_block[ 4096 ];
+	uint8_t empty_block[ 8192 ];
 
 	libbfio_handle_t *file_io_handle = NULL;
 	libcerror_error_t *error         = NULL;
@@ -362,77 +463,90 @@ int evtx_test_check_file_signature_file_io_handle(
 
 	/* Initialize test
 	 */
-	result = libbfio_file_initialize(
-	          &file_io_handle,
-	          &error );
-
-	EVTX_TEST_ASSERT_EQUAL_INT(
-	 "result",
-	 result,
-	 1 );
+	memset_result = memory_set(
+	                 empty_block,
+	                 0,
+	                 sizeof( uint8_t ) * 8192 );
 
 	EVTX_TEST_ASSERT_IS_NOT_NULL(
-	 "file_io_handle",
-	 file_io_handle );
+	 "memset_result",
+	 memset_result );
 
-	EVTX_TEST_ASSERT_IS_NULL(
-	 "error",
-	 error );
+	if( source != NULL )
+	{
+		/* Initialize test
+		 */
+		result = libbfio_file_initialize(
+		          &file_io_handle,
+		          &error );
 
-	source_length = system_string_length(
-	                 source );
+		EVTX_TEST_ASSERT_EQUAL_INT(
+		 "result",
+		 result,
+		 1 );
+
+		EVTX_TEST_ASSERT_IS_NOT_NULL(
+		 "file_io_handle",
+		 file_io_handle );
+
+		EVTX_TEST_ASSERT_IS_NULL(
+		 "error",
+		 error );
+
+		source_length = system_string_length(
+		                 source );
 
 #if defined( HAVE_WIDE_SYSTEM_CHARACTER )
-	result = libbfio_file_set_name_wide(
-	          file_io_handle,
-	          source,
-	          source_length,
-	          &error );
+		result = libbfio_file_set_name_wide(
+		          file_io_handle,
+		          source,
+		          source_length,
+		          &error );
 #else
-	result = libbfio_file_set_name(
-	          file_io_handle,
-	          source,
-	          source_length,
-	          &error );
+		result = libbfio_file_set_name(
+		          file_io_handle,
+		          source,
+		          source_length,
+		          &error );
 #endif
-	EVTX_TEST_ASSERT_EQUAL_INT(
-	 "result",
-	 result,
-	 1 );
+		EVTX_TEST_ASSERT_EQUAL_INT(
+		 "result",
+		 result,
+		 1 );
 
-	EVTX_TEST_ASSERT_IS_NULL(
-	 "error",
-	 error );
+		EVTX_TEST_ASSERT_IS_NULL(
+		 "error",
+		 error );
 
-	result = libbfio_handle_open(
-	          file_io_handle,
-	          LIBBFIO_OPEN_READ,
-	          &error );
+		result = libbfio_handle_open(
+		          file_io_handle,
+		          LIBBFIO_OPEN_READ,
+		          &error );
 
-	EVTX_TEST_ASSERT_EQUAL_INT(
-	 "result",
-	 result,
-	 1 );
+		EVTX_TEST_ASSERT_EQUAL_INT(
+		 "result",
+		 result,
+		 1 );
 
-	EVTX_TEST_ASSERT_IS_NULL(
-	 "error",
-	 error );
+		EVTX_TEST_ASSERT_IS_NULL(
+		 "error",
+		 error );
 
-	/* Test check file signature
-	 */
-	result = libevtx_check_file_signature_file_io_handle(
-	          file_io_handle,
-	          &error );
+		/* Test check file signature
+		 */
+		result = libevtx_check_file_signature_file_io_handle(
+		          file_io_handle,
+		          &error );
 
-	EVTX_TEST_ASSERT_EQUAL_INT(
-	 "result",
-	 result,
-	 1 );
+		EVTX_TEST_ASSERT_EQUAL_INT(
+		 "result",
+		 result,
+		 1 );
 
-	EVTX_TEST_ASSERT_IS_NULL(
-	 "error",
-	 error );
-
+		EVTX_TEST_ASSERT_IS_NULL(
+		 "error",
+		 error );
+	}
 	/* Test error cases
 	 */
 	result = libevtx_check_file_signature_file_io_handle(
@@ -453,8 +567,77 @@ int evtx_test_check_file_signature_file_io_handle(
 
 	/* Clean up
 	 */
-	result = libbfio_handle_close(
+	if( source != NULL )
+	{
+		result = libbfio_handle_close(
+		          file_io_handle,
+		          &error );
+
+		EVTX_TEST_ASSERT_EQUAL_INT(
+		 "result",
+		 result,
+		 0 );
+
+		EVTX_TEST_ASSERT_IS_NULL(
+		 "error",
+		 error );
+
+		result = libbfio_handle_free(
+		          &file_io_handle,
+		          &error );
+
+		EVTX_TEST_ASSERT_EQUAL_INT(
+		 "result",
+		 result,
+		 1 );
+
+		EVTX_TEST_ASSERT_IS_NULL(
+		 "file_io_handle",
+		 file_io_handle );
+
+		EVTX_TEST_ASSERT_IS_NULL(
+		 "error",
+		 error );
+	}
+	/* Test check file signature with data too small
+	 */
+	result = evtx_test_open_file_io_handle(
+	          &file_io_handle,
+	          empty_block,
+	          sizeof( uint8_t ) * 1,
+	          &error );
+
+	EVTX_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+	EVTX_TEST_ASSERT_IS_NOT_NULL(
+	 "file_io_handle",
+	 file_io_handle );
+
+	EVTX_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	result = libevtx_check_file_signature_file_io_handle(
 	          file_io_handle,
+	          &error );
+
+	EVTX_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 -1 );
+
+	EVTX_TEST_ASSERT_IS_NOT_NULL(
+	 "error",
+	 error );
+
+	libcerror_error_free(
+	 &error );
+
+	result = evtx_test_close_file_io_handle(
+	          &file_io_handle,
 	          &error );
 
 	EVTX_TEST_ASSERT_EQUAL_INT(
@@ -466,55 +649,12 @@ int evtx_test_check_file_signature_file_io_handle(
 	 "error",
 	 error );
 
-	result = libbfio_handle_free(
-	          &file_io_handle,
-	          &error );
-
-	EVTX_TEST_ASSERT_EQUAL_INT(
-	 "result",
-	 result,
-	 1 );
-
-	EVTX_TEST_ASSERT_IS_NULL(
-	 "file_io_handle",
-	 file_io_handle );
-
-	EVTX_TEST_ASSERT_IS_NULL(
-	 "error",
-	 error );
-
-	/* Initialize test
+	/* Test check file signature with empty block
 	 */
-	memset_result = memory_set(
-	                 empty_block,
-	                 0,
-	                 sizeof( uint8_t ) * 4096 );
-
-	EVTX_TEST_ASSERT_IS_NOT_NULL(
-	 "memset_result",
-	 memset_result );
-
-	result = libbfio_memory_range_initialize(
+	result = evtx_test_open_file_io_handle(
 	          &file_io_handle,
-	          &error );
-
-	EVTX_TEST_ASSERT_EQUAL_INT(
-	 "result",
-	 result,
-	 1 );
-
-	EVTX_TEST_ASSERT_IS_NOT_NULL(
-	 "file_io_handle",
-	 file_io_handle );
-
-	EVTX_TEST_ASSERT_IS_NULL(
-	 "error",
-	 error );
-
-	result = libbfio_memory_range_set(
-	          file_io_handle,
 	          empty_block,
-	          sizeof( uint8_t ) * 4096,
+	          sizeof( uint8_t ) * 8192,
 	          &error );
 
 	EVTX_TEST_ASSERT_EQUAL_INT(
@@ -522,26 +662,14 @@ int evtx_test_check_file_signature_file_io_handle(
 	 result,
 	 1 );
 
-	EVTX_TEST_ASSERT_IS_NULL(
-	 "error",
-	 error );
-
-	result = libbfio_handle_open(
-	          file_io_handle,
-	          LIBBFIO_OPEN_READ,
-	          &error );
-
-	EVTX_TEST_ASSERT_EQUAL_INT(
-	 "result",
-	 result,
-	 1 );
+	EVTX_TEST_ASSERT_IS_NOT_NULL(
+	 "file_io_handle",
+	 file_io_handle );
 
 	EVTX_TEST_ASSERT_IS_NULL(
 	 "error",
 	 error );
 
-	/* Test check file signature
-	 */
 	result = libevtx_check_file_signature_file_io_handle(
 	          file_io_handle,
 	          &error );
@@ -555,10 +683,8 @@ int evtx_test_check_file_signature_file_io_handle(
 	 "error",
 	 error );
 
-	/* Clean up
-	 */
-	result = libbfio_handle_close(
-	          file_io_handle,
+	result = evtx_test_close_file_io_handle(
+	          &file_io_handle,
 	          &error );
 
 	EVTX_TEST_ASSERT_EQUAL_INT(
@@ -569,25 +695,6 @@ int evtx_test_check_file_signature_file_io_handle(
 	EVTX_TEST_ASSERT_IS_NULL(
 	 "error",
 	 error );
-
-	result = libbfio_handle_free(
-	          &file_io_handle,
-	          &error );
-
-	EVTX_TEST_ASSERT_EQUAL_INT(
-	 "result",
-	 result,
-	 1 );
-
-	EVTX_TEST_ASSERT_IS_NULL(
-	 "file_io_handle",
-	 file_io_handle );
-
-	EVTX_TEST_ASSERT_IS_NULL(
-	 "error",
-	 error );
-
-	/* TODO test file too small */
 
 	return( 1 );
 
@@ -659,27 +766,26 @@ int main(
 	 evtx_test_set_codepage );
 
 #if !defined( __BORLANDC__ ) || ( __BORLANDC__ >= 0x0560 )
-	if( source != NULL )
-	{
-		EVTX_TEST_RUN_WITH_ARGS(
-		 "libevtx_check_file_signature",
-		 evtx_test_check_file_signature,
-		 source );
+
+	EVTX_TEST_RUN_WITH_ARGS(
+	 "libevtx_check_file_signature",
+	 evtx_test_check_file_signature,
+	 source );
 
 #if defined( HAVE_WIDE_CHARACTER_TYPE )
 
-		EVTX_TEST_RUN_WITH_ARGS(
-		 "libevtx_check_file_signature_wide",
-		 evtx_test_check_file_signature_wide,
-		 source );
+	EVTX_TEST_RUN_WITH_ARGS(
+	 "libevtx_check_file_signature_wide",
+	 evtx_test_check_file_signature_wide,
+	 source );
 
 #endif /* defined( HAVE_WIDE_CHARACTER_TYPE ) */
 
-		EVTX_TEST_RUN_WITH_ARGS(
-		 "libevtx_check_file_signature_file_io_handle",
-		 evtx_test_check_file_signature_file_io_handle,
-		 source );
-	}
+	EVTX_TEST_RUN_WITH_ARGS(
+	 "libevtx_check_file_signature_file_io_handle",
+	 evtx_test_check_file_signature_file_io_handle,
+	 source );
+
 #endif /* !defined( __BORLANDC__ ) || ( __BORLANDC__ >= 0x0560 ) */
 
 	return( EXIT_SUCCESS );
