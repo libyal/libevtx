2469ecefab07ecae524c34b49a83dceea143f3c1
Joachim Metz <joachim.metz@gmail.com>
2019-01-04 16:03:22 +0100

    Applied updates

--- libevtx/evtx_event_record.h
+++ libevtx/evtx_event_record.h
@@ -1,7 +1,7 @@
 /*
  * The event record definition of a Windows XML Event Log (EVTX) file
  *
- * Copyright (C) 2011-2018, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2011-2019, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
