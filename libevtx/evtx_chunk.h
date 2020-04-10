9dc8d7911093c58d68b3d8e3c62137db2ca3ee67
Joachim Metz <joachim.metz@gmail.com>
2020-04-10 07:02:05 +0200

    Applied updates

--- libevtx/evtx_chunk.h
+++ libevtx/evtx_chunk.h
@@ -1,7 +1,7 @@
 /*
  * The chunk definition of a Windows XML Event Log (EVTX) file
  *
- * Copyright (C) 2011-2019, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2011-2020, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
--- tests/test_library.ps1
+++ tests/test_library.ps1
@@ -1,6 +1,6 @@
-# Tests C library functions and types.
+# Tests library functions and types.
 #
-# Version: 20181221
+# Version: 20200405
 
 $ExitSuccess = 0
 $ExitFailure = 1
@@ -8,6 +8,7 @@ $ExitIgnore = 77
 
 $LibraryTests = "chunk chunks_table error io_handle notify record record_values template_definition"
 $LibraryTestsWithInput = "file support"
+$OptionSets = "";
 
 $InputGlob = "*"
 
@@ -37,32 +38,32 @@ Function GetTestSetDirectory
 	Return ${TestSetDirectory}
 }
 
-Function GetTestToolDirectory
+Function GetTestExecutablesDirectory
 {
-	$TestToolDirectory = ""
+	$TestExecutablesDirectory = ""
 
-	ForEach (${VSDirectory} in "msvscpp vs2008 vs2010 vs2012 vs2013 vs2015 vs2017" -split " ")
+	ForEach (${VSDirectory} in "msvscpp vs2008 vs2010 vs2012 vs2013 vs2015 vs2017 vs2019" -split " ")
 	{
 		ForEach (${VSConfiguration} in "Release VSDebug" -split " ")
 		{
 			ForEach (${VSPlatform} in "Win32 x64" -split " ")
 			{
-				$TestToolDirectory = "..\${VSDirectory}\${VSConfiguration}\${VSPlatform}"
+				$TestExecutablesDirectory = "..\${VSDirectory}\${VSConfiguration}\${VSPlatform}"
 
-				If (Test-Path ${TestToolDirectory})
+				If (Test-Path ${TestExecutablesDirectory})
 				{
-					Return ${TestToolDirectory}
+					Return ${TestExecutablesDirectory}
 				}
 			}
-			$TestToolDirectory = "..\${VSDirectory}\${VSConfiguration}"
+			$TestExecutablesDirectory = "..\${VSDirectory}\${VSConfiguration}"
 
-			If (Test-Path ${TestToolDirectory})
+			If (Test-Path ${TestExecutablesDirectory})
 			{
-				Return ${TestToolDirectory}
+				Return ${TestExecutablesDirectory}
 			}
 		}
 	}
-	Return ${TestToolDirectory}
+	Return ${TestExecutablesDirectory}
 }
 
 Function ReadIgnoreList
@@ -84,7 +85,7 @@ Function RunTest
 	param( [string]$TestType )
 
 	$TestDescription = "Testing: ${TestName}"
-	$TestExecutable = "${TestToolDirectory}\evtx_test_${TestName}.exe"
+	$TestExecutable = "${TestExecutablesDirectory}\evtx_test_${TestName}.exe"
 
 	$Output = Invoke-Expression ${TestExecutable}
 	$Result = ${LastExitCode}
@@ -111,7 +112,7 @@ Function RunTestWithInput
 	param( [string]$TestType )
 
 	$TestDescription = "Testing: ${TestName}"
-	$TestExecutable = "${TestToolDirectory}\evtx_test_${TestName}.exe"
+	$TestExecutable = "${TestExecutablesDirectory}\evtx_test_${TestName}.exe"
 
 	$TestProfileDirectory = GetTestProfileDirectory "input" "libevtx"
 
@@ -172,11 +173,11 @@ Function RunTestWithInput
 	Return ${Result}
 }
 
-$TestToolDirectory = GetTestToolDirectory
+$TestExecutablesDirectory = GetTestExecutablesDirectory
 
-If (-Not (Test-Path ${TestToolDirectory}))
+If (-Not (Test-Path ${TestExecutablesDirectory}))
 {
-	Write-Host "Missing test tool directory." -foreground Red
+	Write-Host "Missing test executables directory." -foreground Red
 
 	Exit ${ExitFailure}
 }
