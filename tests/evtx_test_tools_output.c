de4826933ac4fd76a0f65529e342c7f81aa364cf
Joachim Metz <joachim.metz@gmail.com>
2021-03-11 05:52:04 +0100

    Worked on tests

--- tests/evtx_test_tools_output.c
+++ tests/evtx_test_tools_output.c
@@ -39,7 +39,7 @@
 
 #include "../evtxtools/evtxtools_output.h"
 
-/* Tests the evtxtools_output_initialize and function
+/* Tests the evtxtools_output_initialize function
  * Returns 1 if successful or 0 if not
  */
 int evtx_test_tools_output_initialize(
