c23e14d09d0e20e1cc8166219e353da6fe64e12f
Joachim Metz <joachim.metz@gmail.com>
2021-05-04 05:13:56 +0200

    Applied updates and added record OSS-Fuzz target

---
+++ ossfuzz/record_fuzzer.cc
@@ -0,0 +1,121 @@
+/*
+ * OSS-Fuzz target for libevtx record type
+ *
+ * Copyright (C) 2011-2020, Joachim Metz <joachim.metz@gmail.com>
+ *
+ * Refer to AUTHORS for acknowledgements.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <stddef.h>
+#include <stdint.h>
+
+/* Note that some of the OSS-Fuzz engines use C++
+ */
+extern "C" {
+
+#include "ossfuzz_libbfio.h"
+#include "ossfuzz_libevtx.h"
+
+#if !defined( LIBEVTX_HAVE_BFIO )
+
+/* Opens a file using a Basic File IO (bfio) handle
+ * Returns 1 if successful or -1 on error
+ */
+LIBEVTX_EXTERN \
+int libevtx_file_open_file_io_handle(
+     libevtx_file_t *file,
+     libbfio_handle_t *file_io_handle,
+     int access_flags,
+     libevtx_error_t **error );
+
+#endif /* !defined( LIBEVTX_HAVE_BFIO ) */
+
+int LLVMFuzzerTestOneInput(
+     const uint8_t *data,
+     size_t size )
+{
+	libbfio_handle_t *file_io_handle = NULL;
+	libevtx_file_t *file              = NULL;
+	libevtx_record_t *record          = NULL;
+	int number_of_records            = 0;
+
+	if( libbfio_memory_range_initialize(
+	     &file_io_handle,
+	     NULL ) != 1 )
+	{
+		return( 0 );
+	}
+	if( libbfio_memory_range_set(
+	     file_io_handle,
+	     (uint8_t *) data,
+	     size,
+	     NULL ) != 1 )
+	{
+		goto on_error_libbfio;
+	}
+	if( libevtx_file_initialize(
+	     &file,
+	     NULL ) != 1 )
+	{
+		goto on_error_libbfio;
+	}
+	if( libevtx_file_open_file_io_handle(
+	     file,
+	     file_io_handle,
+	     LIBEVTX_OPEN_READ,
+	     NULL ) != 1 )
+	{
+		goto on_error_libevtx_file;
+	}
+	if( libevtx_file_get_number_of_records(
+	     file,
+	     &number_of_records,
+	     NULL ) != 1 )
+	{
+		goto on_error_libevtx_file;
+	}
+	if( number_of_records > 0 )
+	{
+		if( libevtx_file_get_record_by_index(
+		     file,
+		     0,
+		     &record,
+		     NULL ) == 1 )
+		{
+			libevtx_record_free(
+			 &record,
+			 NULL );
+		}
+	}
+	libevtx_file_close(
+	 file,
+	 NULL );
+
+on_error_libevtx_file:
+	libevtx_file_free(
+	 &file,
+	 NULL );
+
+on_error_libbfio:
+	libbfio_handle_free(
+	 &file_io_handle,
+	 NULL );
+
+	return( 0 );
+}
+
+} /* extern "C" */
+
