64b1bf847df9830a9e31ead17436d26ff433e845
Joachim Metz <joachim.metz@gmail.com>
2026-09-15 07:29:22 +0200

    Worked on tests and changes for CI tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260609
+# Version: 20260714
 
-if test -f ${PWD}/libevtx/.libs/libevtx.1.dylib && test -f ./pyevtx/.libs/pyevtx.so
+if [ -f "${PWD}/libevtx/.libs/libevtx.1.dylib" ] && [ -f ./pyevtx/.libs/pyevtx.so ]
 then
-	install_name_tool -change /usr/local/lib/libevtx.1.dylib ${PWD}/libevtx/.libs/libevtx.1.dylib ./pyevtx/.libs/pyevtx.so
+    install_name_tool -change /usr/local/lib/libevtx.1.dylib "${PWD}/libevtx/.libs/libevtx.1.dylib" ./pyevtx/.libs/pyevtx.so
 fi
 
 make check-build > /dev/null
 
+# shellcheck disable=SC2068
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0
+if [ ${RESULT} -ne 0 ]
 then
-	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
+    find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
